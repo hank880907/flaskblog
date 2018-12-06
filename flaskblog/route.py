59ea7f554388992e056d7a00a11dd7ae4621d4ce
hank wu <hank880907@gmail.com>
2018-12-07 01:54:48 +0800

    - add edit delet post

--- flaskblog/route.py
+++ flaskblog/route.py
@@ -144,17 +144,49 @@ def save_picture(form_picture):
 @login_required
 def new_post():
     form = PostForm()
-    
     if form.validate_on_submit():
         post = Post(title=form.title.data, content=form.content.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('your post has been created', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title = 'new post', form=form)
+    return render_template('create_post.html', title = 'new post', form=form, legend='What are you thinking now?')
 
 
-@app.route('/post/<int:post_id>', methods=['GET', 'POST'])
+@app.route('/post/<int:post_id>', methods=['GET', 'POST']) # the post_id is defined in the html.
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
+
+
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('your post has been updated', 'success')
+        return redirect(url_for('post', post_id = post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template("create_post.html", title='update post', form=form, legned='Update Post')
+
+
+
+@app.route('/post/<int:post_id>/delete', methods=['POST']) # the post_id is defined in the html.
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('your post has been deleted!', 'success')
+    return redirect(url_for('home'))
